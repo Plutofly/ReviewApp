afc4b6135a5d5df28d4dd4ae9ac2e103e75a168e
Lirimi <beka.lirim@gmail.com>
2021-07-02 16:26:33 +0200

    Changes in Design

--- client/src/components/Auth/auth.js
+++ client/src/components/Auth/auth.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import useStyles from './styles';
-import { Container, Avatar, Button, Paper, Grid, Typography } from '@material-ui/core'
+import { Container, Avatar, Button, Paper, Grid, Typography, Link, Box } from '@material-ui/core'
 import {useParams} from 'react-router'
 
 import Input from './input'
@@ -13,20 +13,19 @@ const Auth = (props) => {
     const handleChange = () => {}
     const handleSubmit = () => {}
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword)
+    const goToTermsAndServices = () => {}
     return (
         <Container component="main" maxWidth="xs">
             <Paper className={classes.paper} elevation={3}>
-                <Avatar className={classes.avatar}>
-                    {/* <LockOutlinedIcon /> */}
-                </Avatar>
-                <Typography variant="h5">{isSignUp ? 'Sign Up' : 'Sign In'}</Typography>
+                <Typography variant="h4" className={classes.mainHeader}>{isSignUp ? 'Sign Up' : 'Sign In'}</Typography>
+                {isSignUp ? (<Box color="text.primary" component="span" m={2} textAlign="center">By continuing you aggre to Reviews <Link href="#" onClick={goToTermsAndServices} variant="body2">{'Terms of Service & Privacy Policy'}</Link>.</Box>) : (<></>) }
                 <form className={classes.form} onSubmit={handleSubmit}>
                     <Grid container spacing={2}>
                         {
                             isSignUp && (
                                 <>
                                     <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
-                                    <Input name="firstName" label="First Name" handleChange={handleChange} half />
+                                    <Input name="firstName" label="Last Name" handleChange={handleChange} half />
 
                                 </>
                             )
@@ -35,7 +34,9 @@ const Auth = (props) => {
                         <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
                         {/* Add Confirm Password later on Signup only*/}
                     </Grid>
-                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                    <Button style={{
+                        background: '#f43939'
+                    }} type="submit" fullWidth color="primary" variant="contained" className={classes.submit}>
                         {isSignUp ? 'Sign Up' : 'Sign In'}
                     </Button>
                 </form>
