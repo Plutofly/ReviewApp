9907b074926e67f3ba204b4217def6ab1ee250ba
Lirimi <beka.lirim@gmail.com>
2021-10-18 00:42:54 +0200

    Revert "Implement Login & Sign Up API"
    This reverts commit d46fab9dbc6cdc340685530c79831194d38fdcd2.

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
-import userRoutes from './routes/users.js';
 
 const app = express();
 
@@ -14,8 +13,8 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-app.use('/user', userRoutes);
-const CONNECTION_URL = 'mongodb+srv://lirimi_31:reviewapp3@cluster0.kjl4w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
